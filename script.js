b42fad3a71017ea5b61660b9a93edc402cda433d
Mocha366 <cerm1240364@gn.iwasaki.ac.jp>
2025-06-11 17:03:09 +0900

    addToDo

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict'
+const table = document.querySelector('table'); //表
+const todo = document.getElementById('todo'); //TODO
+const priority = document.querySelector('select'); //優先度
+const deadline = document.querySelector('imput[type="date"]'); //締め切り
+const submit = document.getElementById('submit'); //登録ボタン
+
+//TODO登録ボタン
+submit.addEventListener('click', () => {
+    const item = {}; //入力値を一時的に格納するオブジェクト
+
+    item.todo = todo.Value;
+    item.priority = priority.value;
+    item.deadline = deadline.value;
+    item.done = false; // 完了はひとまずBoolean値で設定
+
+    console.log(item); // 確認してみる
+
+    // フォームをリセット
+    todo.value = '';
+    priority.value = '普';
+    deadline.value = '';
+
+    const tr = document.createElement('tr'); // tr要素を生成
+
+    // オブジェクトの繰り返しはfor-in文
+    for (const prop in item) {
+        const td = document.createElement('td'); // td要素を生成
+        td.textContent = item[prop]; // ブラケット記法
+        tr.appendChild(td); // 生成したtd要素をtr要素に追加
+    }
+
+    table.append(tr); // trエレメントをtable要素に追加
+});
